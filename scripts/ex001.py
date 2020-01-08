aeb151f4ee6b1c848eade97dc4efdc0d5647387e
peedromarinhow <peedromarinhow@gmail.com>
2020-01-08 11:12:14 -0300

    arquivos base

---
+++ scripts/ex001.py
@@ -0,0 +1,52 @@
+import os
+from PIL import Image
+
+#inpath to images
+inpath = '../../images'
+outpath = '../images'
+
+#open file
+html = open("../photos.html", "w")
+
+html.write('<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<link href = "https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap" rel = "stylesheet">\n\t\t<link href = "main.css" rel = "stylesheet">\n\t\t<title>fotos</title>\n\t</head>\n\t<body>\n\t\t<div class = "row">')
+
+#navigate all files in dir
+for filename in os.listdir(inpath):
+
+    #open each image if it's a jpeg
+    if ".jpeg" in filename:
+        picture = Image.open(inpath + "/" + filename)
+
+        #set the scale factor
+        size = int(picture.size[0] / 2), int(picture.size[1] / 2)
+
+        #resize image
+        picture.thumbnail(size)
+
+        #get metadata of image
+        data = picture._getexif()[36867]
+
+        #get date of image and format it
+        day = str(data)[8:10]
+        month = str(data)[5:7]
+        year = str(data)[:4]
+
+        date = day + "/" + month + "/" + year
+
+        #save image
+        picture.save(os.path.join(outpath, filename))
+
+        #create tag and write
+        html.write(f"\n\t\t\t<div><div class = \"image\"><img src = \"images\\{filename}\" class = \"center\"></div><p> {date} </p></div>")
+
+        #print its filename
+        print(filename)
+
+#join final code
+html.write("\n\t\t</div>\n\t</body>\n</html>")
+
+#close file
+html.close()
+
+#close picture
+picture.close()
---
+++ scripts/ex002.py
@@ -0,0 +1,69 @@
+#image scrapper from imgur user gallery
+#
+#   This script gets all the images from
+#   a user's imgur gallery and embeds them
+#   into a row of html elements sorted by
+#   date and time.
+#
+import configparser
+import imgurpython
+import time
+from datetime import datetime
+
+#set up our Oauth infotmation
+config = configparser.ConfigParser()
+config.read("auth.ini")
+
+#get the Oauth information
+client_id     = config.get("credentials", "client_id")
+client_secret = config.get("credentials", "client_secret")
+
+#get user information
+imgur_username = config.get("credentials", "imgur_username")
+imgur_password = config.get("credentials", "imgur_password")
+
+#define the client
+client = imgurpython.ImgurClient(client_id, client_secret)
+
+#get the autorization url
+authorization_url = client.get_auth_url("pin")
+
+print(authorization_url)
+
+#get pin
+imgur_pin = input("pin :")
+
+#authorize
+credentials = client.authorize(imgur_pin, "pin")
+client.set_user_auth(credentials["access_token"], credentials["refresh_token"])
+
+#get images
+images = client.get_account_images(imgur_username)
+
+#reserve dict
+images_dict = []
+
+#store imagesn in dict by name and creation
+for image in images:
+
+    #get the day  and time the image was shot from name (canon sx60hs only)
+    name = image.name
+    date = name[:4] + '/' + name[4:6] + '/' + name[6:8]
+    time = name[8:10] + ':' + name[10:12] + ':' + name[12:14]
+
+    #store the images by name(url id) and datetime
+    images_dict.append({"name": image.id, "created": date + ' ' + time})
+    print({"name": image.id, "created": date + ' ' + time})
+
+#sort images by date and time
+sorted_images = sorted(
+    images_dict,
+    key=lambda x: datetime.strptime(x["created"], "%Y/%m/%d %H:%M:%S")
+)
+
+#print two newlines
+print(); print()
+
+#print the html elements for copying
+for image in sorted_images:
+    print(f"""<div><div class = "image"><img src = https://i.imgur.com/{image["name"]}h.jpg class = "center"></div><p> {image["created"]} </p></div>""")
