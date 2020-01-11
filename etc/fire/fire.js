26f33df02d78b5fa67fa3f24970e8cda16f7efc7
peedromarinhow <peedromarinhow@gmail.com>
2020-01-11 15:13:39 -0300

    adicionado fogo e mudanças menores

--- etc/fire/fire.js
+++ etc/fire/fire.js
@@ -1,117 +1,98 @@
-const colors = [{"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}]
-const pixels = []
+const firePixels   = []
+const colorPallete = [{"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}]
 
-let width = 10
-let height = 5
+const width  = 25
+const height = 70
 
-//get the size of the array
 const pixelCount = width * height
 
-function start()
+function main()
 {
     doFireStruct()
     doFireSource()
 
-    doFirePropagation()
+    setInterval(doFirePropagation, 50)
 }
 
-//generate data structure for the fire
 function doFireStruct()
 {
-    //iterate over all pixels and set them to 0
     for (let i = 0; i < pixelCount; i++)
     {
-        pixels[i] = 0
+        firePixels[i] = 0
     }
 }
 
-//generate the fire source
 function doFireSource()
 {
-    for (let column = 0; column <= width; column++) {
-        const overflowIndex = width * height
-        const index = (overflowIndex - width) + column
-    
-        pixels[index] = 36
-      }
-}
-
-//render the fire to the html file
-function doFireRender()
-{
-    //set the first line of the html table
-    let html = "<table cellpadding = 0 cellspacing = 0>"
-
-    //iterate over rows
-    for (let row = 0; row < height; row++)
+    for (let column = pixelCount - 1; column >= pixelCount - width; column--)
     {
-        //for each row add a table row
-        html += "<tr>"
-
-        //iterate over columns
-        for (let column = 0; column < width; column++)
-        {
-            //set the index, intensty, and color of the current pixel
-            const index     = column + (width * row)
-            const intensity = pixels[index]
-            const color     = colors[intensity]
-            const colorStr  = `${color.r},${color.g},${color.b}`
-
-            //add the pixel to the html
-            html += `<td class="pixel" style="backround-color: rgb(${colorStr})">`
-            html += "</td>"
-        }
-
-        //close the table row
-        html += "</tr>"
+        firePixels[column] = 36
     }
-
-    //close the table
-    html += "</table>"
-
-    //add to main file
-    document.querySelector("#fireplace").innerHTML = html
 }
 
-//calculate fire propagation
 function doFirePropagation()
 {
-    //iterate over columns
     for (let column = 0; column < width; column++)
     {
-        //iterate over rows
-        for (let row = 0; row < height; row++)
-        {
-            //set the index
-            const index = column + (width * row)
-
-            //update intensity
-            updateIntensityPerPixel(index)
+        for (let row = 0; row < height; row++) {
+            const index = column + ( width * row )
+            doUpdateFireIntensity(index)
         }
     }
 
-    //render the fire
-    doFireRender()
+    doFireRender(false)
 }
 
-//calculate the intensity per pixel
-function updateIntensityPerPixel(index)
+function doUpdateFireIntensity(index)
 {
-    //get the below pixel
     const below = index + width
 
-    //return if out of range
     if (below >= pixelCount)
         return
 
-    //get a random decay from Math
     const decay = Math.floor(Math.random() * 2)
-
-    //set the new intensity
-    const belowIntensity = pixels[below]
+    const belowIntensity = firePixels[below]
     const newIntensity   = belowIntensity - decay >= 0 ? belowIntensity - decay : 0
 
-    pixels[index - decay] = newIntensity
+    firePixels[index - decay] = newIntensity
 }
 
-start()
+function doFireRender(debug)
+{
+    let html = "<table cellpadding = 0 cellspacing = 0>"
+
+    for (let row = 0; row < height; row++)
+    {
+        html += "<tr>"
+
+        for (let column = 0; column < width; column++)
+        {
+            const index = column + ( width * row )
+            const intensity = firePixels[index]
+
+            if (debug === true)
+            {
+                html += "<td>"
+                html += `<div class = "pixel-index">${index}</div>`
+                html += intensity
+                html += "</td>"
+            }
+            else
+            {
+                const color    = colorPallete[intensity]
+                const colorStr = `${color.r},${color.g},${color.b}`
+
+                html += `<td class = "pixel" style = "background-color: rgb(${colorStr})"`
+                html += "</td>"
+            }
+        }
+
+        html += "</tr>"
+    }
+
+    html += "</table>"
+
+    document.querySelector("#fireplace").innerHTML = html
+}
+
+main()
