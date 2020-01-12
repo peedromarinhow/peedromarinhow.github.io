a0b776cb1212744778a633f05d32af9400dad27f
peedromarinhow <peedromarinhow@gmail.com>
2020-01-11 21:29:24 -0300

    alteradas cores para melhor visibilidade

--- etc/fire/fire.js
+++ etc/fire/fire.js
@@ -1,8 +1,10 @@
-const firePixels   = []
+let firePixels   = []
 const colorPallete = [{"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}]
 
-const width  = 25
-const height = 70
+let width  = 25
+let height = 70
+
+let debug  = false
 
 const pixelCount = width * height
 
@@ -40,7 +42,7 @@ function doFirePropagation()
         }
     }
 
-    doFireRender(false)
+    doFireRender()
 }
 
 function doUpdateFireIntensity(index)
@@ -57,7 +59,7 @@ function doUpdateFireIntensity(index)
     firePixels[index - decay] = newIntensity
 }
 
-function doFireRender(debug)
+function doFireRender()
 {
     let html = "<table cellpadding = 0 cellspacing = 0>"
 
@@ -73,8 +75,7 @@ function doFireRender(debug)
             if (debug === true)
             {
                 html += "<td>"
-                html += `<div class = "pixel-index">${index}</div>`
-                html += intensity
+                html += firePixels[index]
                 html += "</td>"
             }
             else
@@ -93,6 +94,18 @@ function doFireRender(debug)
     html += "</table>"
 
     document.querySelector("#fireplace").innerHTML = html
+}
+
+function doToggleDebug()
+{
+    if (debug === false)
+    {
+        debug  = true
+    }
+    else
+    {
+        debug  = false
+    }
 }
 
 main()
